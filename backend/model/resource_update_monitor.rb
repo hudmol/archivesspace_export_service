7904c47390512d7de79c0169232d6bcf37c639a5
jambun <jambun@gmail.com>
2016-04-20 14:41:41 +1000

    update-feed endpoint working for basic case

--- backend/model/resource_update_monitor.rb
+++ backend/model/resource_update_monitor.rb
@@ -4,7 +4,32 @@ class ResourceUpdateMonitor
   end
 
   def updates_since(timestamp)
-    {"adds" => [1, 2, 3], "removes" => [4, 5]}
+    adds = []
+    removes = []
+    mtime = Time.at(timestamp.to_i)
+    DB.open do |db|
+      mods = db[:resource].where(Sequel.qualify(:resource, :system_mtime) > mtime)
+        .select(:id, :identifier, :publish, :suppressed)
+
+      mods.each do |res|
+        if res[:publish] == 1 && res[:suppressed] == 0
+          adds << {'id' => res[:id], 'identifier' => JSON.parse(res[:identifier])}
+        else
+          removes << res[:id]
+        end
+      end
+
+      dels = db[:deleted_records].where(Sequel.qualify(:deleted_records, :timestamp) > mtime)
+        .select(:uri)
+
+      dels.each do |res|
+        removes << res[:uri].split('/')[-1]
+      end
+
+    end
+
+
+    {'timestamp' => timestamp, 'adds' => adds, 'removes' => removes}
   end
 
 end
