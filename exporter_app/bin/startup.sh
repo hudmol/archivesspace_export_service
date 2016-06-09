68c68027af1947215a2d719caffc1a7a2d972433
jambun <jambun@gmail.com>
2016-06-09 11:56:40 +1000

    explicitly set https protocols

--- exporter_app/bin/startup.sh
+++ exporter_app/bin/startup.sh
@@ -5,5 +5,5 @@ BASEDIR=$(dirname "$0")/../
 (
     cd "$BASEDIR"
     mkdir -p logs
-    java $JAVA_OPTS -Darchivesspace-exporter=yes -Dfile.encoding=UTF-8 -cp "bin/*:java_lib/*:$CLASSPATH" org.jruby.Main -- exporter_app.rb 2>logs/exporter_app.err
+    java $JAVA_OPTS -Darchivesspace-exporter=yes -Dfile.encoding=UTF-8 -Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2 -cp "bin/*:java_lib/*:$CLASSPATH" org.jruby.Main -- exporter_app.rb 2>logs/exporter_app.err
 )
