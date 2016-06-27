96762bf2cab3f97e18cfcdc0fdf2bc80d2e5403c
Mark Triggs <mark@dishevelled.net>
2016-06-27 16:07:33 +1000

    Use exec so we don't leave an instance running on Ctrl-\

--- exporter_app/bin/startup.sh
+++ exporter_app/bin/startup.sh
@@ -2,8 +2,6 @@
 
 BASEDIR=$(dirname "$0")/../
 
-(
-    cd "$BASEDIR"
-    mkdir -p logs
-    java $JAVA_OPTS -Darchivesspace-exporter=yes -Dfile.encoding=UTF-8 -cp "bin/*:java_lib/*:$CLASSPATH" org.jruby.Main -- exporter_app.rb 2>logs/exporter_app.err
-)
+cd "$BASEDIR"
+mkdir -p logs
+exec java $JAVA_OPTS -Darchivesspace-exporter=yes -Dfile.encoding=UTF-8 -cp "bin/*:java_lib/*:$CLASSPATH" org.jruby.Main -- exporter_app.rb 2>logs/exporter_app.err
